666a4179a89f913b266447436960ee38c43c03e9
ilya rudenko <webilya29@gmail.com>
2021-06-15 15:37:06 +0300

    Add game scene

--- Millionaire/ViewController.swift
+++ Millionaire/ViewController.swift
@@ -8,12 +8,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let segueToGameVC = "ToGameVC"
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    
+    let questionCount = getQuestions().count
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func didTapNewGame(_ sender: UIButton) {
+        performSegue(withIdentifier: segueToGameVC, sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch segue.identifier {
+        case segueToGameVC:
+            if let destination = segue.destination as? GameViewController {
+                destination.delegate = self
+            }
+        default:
+            break
+        }
+    }
+}
 
-
+extension ViewController: GameViewControllerDelegate {
+    func didEndGame(with result: Int) {
+        scoreLabel.text = "Последний результат: \(result) из \(questionCount)"
+    }
 }
 
